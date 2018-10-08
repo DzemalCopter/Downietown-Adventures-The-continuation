379e8b6ec6d8495ee4ada01895cc77d119326d8c
LordAru <arujan.gnaneswaran@outlook.de>
2018-10-08 14:36:46 +0200

    Level

---
+++ [04]/src/model/Level.java
@@ -0,0 +1,45 @@
+package model;
+
+public class Level {
+
+    private Auftrag[] auftrag;
+    private String[] scenario;
+    private String question;
+    private String correctAnswer;
+
+    public Level(int level) {
+        createAuftrag(level);
+    }
+
+    private void createAuftrag(int level){
+        scenario = new String[4];
+        switch(level){
+            case 1: auftrag = new Auftrag[2];
+                auftrag[0] = new Auftrag("Du bist der Boss einer Firma und sitzt an einem schönen, sonnigen Tag in deinem Büro und schaust in die Firmenrechnung. Aufeinmal merkst du, dass dein Manager Geld veruntreut  hat. Was machst du?",new String[]{"Töten","Steuererklärung schreiben","Streiten"});
+                auftrag[1] = new Auftrag("Dein Chef hat zum dritten mal an diesem Tag deinen Kaffee geklaut. Deine Aggression verleitet dich zu...",new String[]{"Töten","Steuererklärung schreiben","Streiten"});
+                break;
+            case 2: auftrag = new Auftrag[2];
+                auftrag[0] = new Auftrag("Du unterhälst dich mit einer Arbeitskollegin und sie 'friendzoned' dich. Du bist traurig. Was willst du tun?",new String[]{"Töten","Steuererklärung schreiben","Streiten"});
+                auftrag[1] = new Auftrag("Dein Kollege verabschiedet sich mit den Worten 'Bruder muss Los', obwohl er weiß, dass er noch eine Steuererklärung machen muss.",new String[]{"Töten","Steuererklärung schreiben","Streiten"});
+                break;
+            case 3: auftrag = new Auftrag[2];
+                auftrag[0] = new Auftrag("Du entdeckst einen korrupten Mitarbeiter in der Firma und der Chef weiß noch nichts davon. Was willst du tun?",new String[]{"Töten","Steuererklärung schreiben","Streiten"});
+                auftrag[1] = new Auftrag("Die Sekretärin hat deinen Namen auf deiner Steuererklärung falsch geschrieben. Du bist wütend und willst...",new String[]{"Töten","Steuererklärung schreiben","Streiten"});
+                break;
+            case 4:
+               auftrag = new Auftrag[2];
+                auftrag[0] = new Auftrag("",new String[]{"Töten","Steuererklärung schreiben","Streiten"});
+                auftrag[1] = new Auftrag("",new String[]{"Töten","Steuererklärung schreiben","Streiten"});
+                break;
+            case 5:
+                auftrag = new Auftrag[2];
+                auftrag[0] = new Auftrag("",new String[]{"Töten","Steuererklärung schreiben","Streiten"});
+                auftrag[1] = new Auftrag("",new String[]{"Töten","Steuererklärung schreiben","Streiten"});
+        }
+    }
+
+    public Auftrag getRandomAuftrag(){
+        int r= (int) (Math.random()* auftrag.length);
+        return auftrag[r];
+    }
+}
